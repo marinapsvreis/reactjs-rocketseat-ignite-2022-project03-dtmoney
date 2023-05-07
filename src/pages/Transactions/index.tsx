e8e0da27b747730f32dc12da67a0db34ef00a17a
Marina Portugal <marinapsvreis@gmail.com>
2023-05-07 10:13:26 -0300

    :art: Formatando valores monetários e datas

--- src/pages/Transactions/index.tsx
+++ src/pages/Transactions/index.tsx
@@ -2,6 +2,7 @@ import { useContext, useEffect, useState } from "react";
 import { Header } from "../../components/Header";
 import { Summary } from "../../components/Summary";
 import { TransactionsContext } from "../../contexts/TransactionsContext";
+import { dateFormatter, priceFormatter } from "../../utils/formatter";
 import { SearchForm } from "./components/SearchForm";
 import {
   PriceHighlight,
@@ -19,7 +20,7 @@ interface Transaction {
 }
 
 export function Transactions() {
-  const {  transactions } = useContext(TransactionsContext);
+  const { transactions } = useContext(TransactionsContext);
 
   return (
     <div>
@@ -35,11 +36,12 @@ export function Transactions() {
                 <td width="50%">{transaction.description}</td>
                 <td>
                   <PriceHighlight variant={transaction.type}>
-                    {transaction.price}
+                    {transaction.type === "outcome" && "- "}
+                    {priceFormatter.format(transaction.price)}
                   </PriceHighlight>
                 </td>
                 <td>{transaction.category}</td>
-                <td>{transaction.createdAt}</td>
+                <td>{dateFormatter.format(new Date(transaction.createdAt))}</td>
               </tr>
             ))}
           </tbody>
