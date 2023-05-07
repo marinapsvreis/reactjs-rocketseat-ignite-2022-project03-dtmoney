2288e7cb79efdd6d74b577395ca45042aeb0166e
Marina Portugal <marinapsvreis@gmail.com>
2023-05-07 10:43:47 -0300

    :safety_vest: Usando react hook form com zod para validar os formularios

--- src/pages/Transactions/components/SearchForm/styles.ts
+++ src/pages/Transactions/components/SearchForm/styles.ts
@@ -24,14 +24,19 @@ export const SearchFormContainer = styled.form`
 
     border: 0;
     padding: 1rem;
-
     background: transparent;
     border: 1px solid ${(props) => props.theme["green-300"]};
     color: ${(props) => props.theme["green-300"]};
     font-weight: bold;
     border-radius: 6px;
+    cursor: pointer;
 
-    &:hover {
+    &:disabled {
+      opacity: 0.6;
+      cursor: not-allowed;
+    }
+
+    &:not(:disabled):hover {
       background: ${(props) => props.theme["green-500"]};
       border-color: ${(props) => props.theme["green-500"]};
       color: ${(props) => props.theme.white};
